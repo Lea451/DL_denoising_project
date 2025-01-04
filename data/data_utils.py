29317a983a567f492db68888fe35a74115941f49
valentine.tosel <valentine.tosel@polytechnique.edu>
2025-01-04 12:10:30 +0100

    post_proc maj, remis calcul de phase des spectos

--- data/data_utils.py
+++ data/data_utils.py
@@ -95,9 +95,9 @@ def generate_global_specto(dir_path, name, output_folder):
     """
     Input : directory path (in which two subdirectories 'train' and 'test'), 
             name of the output file
-    Save two arrays :
+    Save one array :
         spectos = array with all the [noisy spectogram amplitude, clean spectogram amplitude]
-        specto_noisy_phase = array of the angles of noisy spectograms
+        phases = array with the angles of the noisy spectograms
     """
     spectos = []
     spectos_angles = []
@@ -114,20 +114,19 @@ def generate_global_specto(dir_path, name, output_folder):
             longueur = len(noisy_signal)
             _, _, specto_clean = ss.stft(clean_signal, 1, nperseg=longueur//20, noverlap=longueur//40)
             _, _, specto_noisy = ss.stft(noisy_signal, 1, nperseg= longueur//20, noverlap=longueur//40)
+            specto_noisy_phase = np.angle(specto_noisy)
             specto_clean = np.abs(specto_clean)
             specto_noisy = np.abs(specto_noisy)  #comme dans l'article Unet, on utilise uniquement les magnitudes du spectogramme A MODIFIER SI BESOIN
-            specto_noisy_phase = np.angle(specto_noisy)
-            spectos_angles.append(specto_noisy_phase)
-            spectos.append([specto_noisy, specto_clean]) 
+            spectos.append([specto_noisy, specto_clean])
+            spectos_angles.append(specto_noisy_phase) 
     np.save(output_path_spectos, np.array(spectos))
-    np.save(output_path_phases, np.array(specto_noisy_phase))
+    np.save(output_path_phases, np.array(spectos_angles))
+    
 #TO DO ONCE
 #generate_global_specto(r"C:\Users\valen\Documents\Travail\X\MVA\S1\ProjetTDS\DL_denoising_project\data\train",'train',r"C:\Users\valen\Documents\Travail\X\MVA\S1\ProjetTDS\DL_denoising_project\data\global")
 #generate_global_specto(r"C:\Users\valen\Documents\Travail\X\MVA\S1\ProjetTDS\DL_denoising_project\data\test",'test',r"C:\Users\valen\Documents\Travail\X\MVA\S1\ProjetTDS\DL_denoising_project\data\global")
-#generate_global_specto(r"./data/train",'train', r"./data/global")
-#generate_global_specto(r"./data/test",'test',r"./data/global")
+generate_global_specto(r"./data/train",'train', r"./data/global")
+generate_global_specto(r"./data/test",'test',r"./data/global")
 
-specto = np.load(r"./data/global/train_spectos.npy")
-print(specto.shape)
 
 
